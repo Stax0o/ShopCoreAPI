bfb37132470e1f7daeabc386b73bd1d11a5fda64
Artem Aliev <stax0o.devr@gmail.com>
2025-07-17 10:40:44 +0500

    init

---
+++ src/main/java/com/github/stax0o/shopcoreapi/ShopCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.stax0o.shopcoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopCoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/stax0o/shopcoreapi/ShopCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stax0o.shopcoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
